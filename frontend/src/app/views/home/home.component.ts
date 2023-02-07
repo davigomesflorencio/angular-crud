f62dac797dd5d91d1286afb48db931079fe9d24d
davigomesflorencio <davigomesflorencio@gmail.com>
2023-02-07 15:36:48 -0300

    feat - Inserindo serviço de alteração de titulo nas tela principal

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { HeaderService } from "./../../components/template/header/header.service";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
-
+  constructor(private headerService: HeaderService) {
+    headerService.headerData = {
+      title: "Inicio",
+      icon: "home",
+      routeUrl: "",
+    };
+  }
 }
--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,6 +1,7 @@
 import { OnInit } from "@angular/core";
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { HeaderService } from "src/app/components/template/header/header.service";
 
 @Component({
   selector: "app-product-crud",
@@ -8,8 +9,13 @@ import { Router } from "@angular/router";
   styleUrls: ["./product-crud.component.css"],
 })
 export class ProductCrudComponent implements OnInit {
-  constructor(private router: Router) {}
-
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: "Cadastro de produtos",
+      icon: "storefront",
+      routeUrl: "/products",
+    };
+  }
   ngOnInit(): void {}
   navigateProductCreate(): void {
     this.router.navigate(["/products/create"]);
